5b2d7ddca5158b8e5b7036c32e8f9e7c45837112
Attila Nyiri <Attila.Nyiri@quest.com>
2023-05-16 09:03:04 +0200

    Added UpdateMovieRequest, MovieResponse and MoviesResponse

---
+++ src/Movies/Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.Contracts.Responses;
+
+public record MoviesResponse
+{
+    public required IEnumerable<MovieResponse> Movies { get; init; } = Enumerable.Empty<MovieResponse>();
+}
