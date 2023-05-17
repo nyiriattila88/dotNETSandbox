0feb22cfce352ad6456adf2f263dce27ebb7eaa1
Attila Nyiri <Attila.Nyiri@quest.com>
2023-05-17 15:40:33 +0200

    Added MoviesController

---
+++ src/Movies/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        Movie movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+
+        MovieResponse movieResponse = movie.MapToMovieResponse();
+        return Created($"api/movies/{movieResponse.Id}", movieResponse);
+    }
+}
