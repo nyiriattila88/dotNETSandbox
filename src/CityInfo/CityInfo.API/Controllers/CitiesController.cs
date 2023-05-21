409ac2fee0a14465f22482d23e0cbc6bb8456046
Attila Nyiri <Attila.Nyiri@quest.com>
2023-05-21 22:59:02 +0200

    Changed CitiesController GetCities

--- src/CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ src/CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -6,10 +6,5 @@ namespace CityInfo.API.Controllers;
 public class CitiesController : ControllerBase
 {
     [HttpGet(ApiEndpoints.Cities.GetAll)]
-    public JsonResult GetCities() => new(
-            new List<object>
-            {
-                new {id = 1, name = "New York City"},
-                new {id = 2, name = "Antwerp"},
-            });
+    public JsonResult GetCities() => new JsonResult(CitiesDataStore.Instance.Cities);
 }
