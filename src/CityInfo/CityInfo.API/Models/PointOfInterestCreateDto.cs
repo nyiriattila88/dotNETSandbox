e8c53f34ad5293cb87be90d9a2e5dec0353218c9
Attila Nyiri <Attila.Nyiri@quest.com>
2023-05-24 08:28:36 +0200

    Added RequiredAttribute to Dto properties

--- src/CityInfo/CityInfo.API/Models/PointOfInterestCreateDto.cs
+++ src/CityInfo/CityInfo.API/Models/PointOfInterestCreateDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CityInfo.API.Models;
 
 public record PointOfInterestCreateDto
 {
+    [Required(ErrorMessage = $"{nameof(Name)} is required.")]
+    [MaxLength(50)]
     public required string Name { get; set; }
 
+    [MaxLength(200)]
     public string? Description { get; set; }
 }
--- src/CityInfo/CityInfo.API/Models/PointOfInterestDto.cs
+++ src/CityInfo/CityInfo.API/Models/PointOfInterestDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CityInfo.API.Models;
 
 public record PointOfInterestDto
 {
+    [Required(ErrorMessage = $"{nameof(Id)} is required.")]
     public required int Id { get; set; }
 
+    [Required(ErrorMessage = $"{nameof(Name)} is required.")]
     public required string Name { get; set; }
 
     public string? Description { get; set; }
